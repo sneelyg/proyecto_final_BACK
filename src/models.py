31fd9fc96aed101637bf52698165ccdf5a9892f9
sneelyg <santiagoneely@gmail.com>
2022-09-30 22:50:17 +0000

    empezando api

--- src/models.py
+++ src/models.py
@@ -5,8 +5,12 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    direccion = db.Column(db.String(120), unique=True, nullable=False)
+    marca = db.Column(db.String(120), unique=True, nullable=False)
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +19,6 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "username" : self.username
             # do not serialize the password, its a security breach
         }
