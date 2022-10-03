0a7f075f9f0df90103add351a80acdf4d3028b4a
sneelyg <santiagoneely@gmail.com>
2022-10-03 23:50:36 +0000

    Rutas Creadas en Api y Modelo Listo

--- src/main.py
+++ src/main.py
@@ -8,7 +8,9 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Marca, Producto
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import datetime
 #from models import Person
 
 app = Flask(__name__)
@@ -19,6 +21,7 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+jwt = JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -38,6 +41,122 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route ('/login', methods = ['POST'])
+def login():
+    body = request.get_json()  #Esto hace que e lbody que envia la api sea leido como json.
+    one = User.query.filter_by(email=body['email'], password = body['password']).first() #Esto compara el "email" y "password" que llegó desde el body con los de la tabla User.
+    if (one is None): 
+        raise APIException("Ususario no existe o clave incorrecta", status_code=401)
+    else :
+        expiracion = datetime.timedelta(minutes=60)
+        acceso = create_access_token(identity= body['email'], expires_delta = expiracion)  
+        return {
+            "login" : "ok",
+            "token" : acceso,
+            "tiempo" : expiracion.total_seconds()
+        }  
+
+@app.route ('/registro', methods = ['POST'])
+def registro():
+    body = request.get_json()
+    #Primero veo que usuario no exista
+    existe_mail = User.query.filter_by(email = body['email']).first()
+    if (existe is  not None):
+        return "El correo utilizado ya existe en la base de datos"
+    
+    existe_username = User.query.filter_by(username = body['username']).first()
+    if (existe_username is  not None):
+        return "El nombre de usuario utilizado ya existe en la base de datos"
+
+    else :
+            
+        new_user = User()
+        new_user.email = body['email']
+        new_user.password = body['password']
+        new_user.is_active = True
+        new_user.username = body['username']
+        new_user.nombre = body['nombre']
+        new_user.apellido = body['apellido']
+        new_user.rut = body['rut']
+
+        db.session.add(new_user)
+        db.session.commit()
+        return "Usuario Nuevo Creado" 
+###
+{
+"email": "santiagoneely@123.cl",
+    "password" : "123456",
+    "is_active" : "True",
+    "username":"Sneely",
+    "nombre":"Santi",
+    "apellido":"neely",
+    "rut":"12345"
+}
+
+###
+@app.route ('/marcas/registro', methods = ['POST'])
+def marcas_registro():
+    body = request.get_json()
+    #Primero veo que usuario no exista
+    existe_marca = Marca.query.filter_by(nombre_marca = body['nombre_marca']).first()
+    if (existe_marca is  not None):
+        return "El nombre de marca ya existe"
+
+    else:
+        new_marca = Marca()
+        new_marca.vendedor = body ['vendedor']
+        new_marca.nombre_marca = body ['nombre_marca']
+        new_marca.direccion = body ['direccion']
+        new_marca.descripcion = body ['descripcion']
+        new_marca.tipo_pago = body ['tipo_pago']
+        new_marca.banco_cuenta = body ['banco_cuenta']
+        new_marca.tipo_cuenta = body ['tipo_cuenta']
+        new_marca.numero_cuenta = body ['numero_cuenta']
+        new_marca.rut_cuenta = body ['rut_cuenta']
+
+        db.session.add(new_marca)
+        db.session.commit()
+        return "Nueva Marca Creada " 
+
+@app.route ('/productos/registro', methods = ['POST'])
+def productos_registro():
+    return "Registrar Producto"
+
+@app.route ('/recuperar_clave', methods = ['POST'])
+def recuperar_clave():    
+    return "Recuperar Clave"
+
+
+@app.route('/marcas/<int:id_marca>', methods=['GET'])
+def una_marca(id_marca):
+    return "Una Marca"
+
+@app.route('/productos/<int:id_producto>', methods=['GET'])
+def un_producto(id_producto):
+    return "Un Producto"
+
+@app.route('/marcas', methods=['GET'])
+def todas_las_marcas():
+    return "Todas las  Marcas"
+
+@app.route('/productos', methods=['GET'])
+def todos_los__productos():
+    return "Todos los  Productos"
+
+
+@app.route('/productos/<int:id_producto>', methods=['DELETE'])
+def borrar_un_producto(id_producto):
+    return "Borrar Un Producto"
+
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,6 +7,12 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
+    apellido = db.Column(db.String(120), unique=False, nullable=False)
+    rut = db.Column(db.Integer, unique=True, nullable=False)
+
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +21,55 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "usuario" : self.username,
+            "nombre" : self.nombre,
+            "apellido":self.apellido
             # do not serialize the password, its a security breach
+        }
+
+class Marca (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vendedor =  db.Column(db.Integer, db.ForeignKey('user.id'))
+    nombre_marca = db.Column(db.String(80), unique=True, nullable=False)
+    direccion = db.Column(db.String(250), unique=False, nullable=False)
+    descripcion = db.Column(db.String(250), unique=False, nullable=False)
+    tipo_pago = db.Column(db.String(250), unique=False, nullable=False)
+    banco_cuenta = db.Column(db.String(250), unique=False, nullable=True)
+    tipo_cuenta = db.Column(db.String(50), unique=False, nullable=True)
+    numero_cuenta = db.Column(db.Integer, unique=True, nullable=True)
+    rut_cuenta = db.Column(db.String(120), unique=False, nullable=False)
+   
+    rel_user = db.relationship('User')
+   
+    def __repr__(self):
+        return '<Marca %r>' % self.nombre_marca
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "marca": self.nombre_marca,
+            "descricpion":self.descripcion
+            # do not serialize the password, its a security breach
+        }
+
+class Producto (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vendedor = db.Column(db.Integer, db.ForeignKey('user.id'))
+    marca = db.Column(db.Integer, db.ForeignKey('marca.id'))
+    nombre_producto = db.Column(db.String(250), unique=False, nullable=False)
+    descripcion = db.Column(db.String(250), unique=False, nullable=False)
+    precio =  db.Column(db.Integer, unique=True, nullable=False)
+    url_foto = db.Column(db.String(250), unique=True, nullable=True)
+    rel_user = db.relationship('User')
+    rel_marca = db.relationship('Marca')
+
+    def __repr__(self):
+        return '<Producto %r>' % self.nombre_producto
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "producto": self.nombre_producto,
+            "descricpion":self.descripcion,
+            "precio": self.precio
         }
